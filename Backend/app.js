6b2dd736bc3f382d0a06991b55b621fd3fa93b11
PrathamKhandelwal1301 <111449187+PrathamKhandelwal1301@users.noreply.github.com>
2024-08-14 21:12:54 +0530

    manually changing app.js due to conflicts

--- Backend/app.js
+++ Backend/app.js
@@ -8,17 +8,53 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	console.log(req.ip);
-	res.send(req.ip);
+	console.log('pinged');
+	res.send('Working');
 });
 
-app.get('/api/sendmail', (req, res) => {
+app.post('/api/sendmail', (req, res) => {
 	const { userEmail, mailBody } = req.body;
-	// Create a transporter using Outlook's SMTP settings
+	const cart = req.body.cart;
+
+	let cartHtml = `
+    <h2 style="font-family: Arial, sans-serif; color: #333;">Your Walmart Cart</h2>
+    <table style="width: 100%; border-collapse: collapse; font-family: Arial, sans-serif;">
+      <thead>
+        <tr style="background-color: #f2f2f2; text-align: left;">
+          <th style="padding: 12px; border-bottom: 1px solid #ddd;">Item</th>
+          <th style="padding: 12px; border-bottom: 1px solid #ddd;">Quantity</th>
+          <th style="padding: 12px; border-bottom: 1px solid #ddd;">Amount</th>
+        </tr>
+      </thead>
+      <tbody>`;
+
+	cart.forEach((item) => {
+		cartHtml += `
+        <tr>
+          <td style="padding: 12px; border-bottom: 1px solid #ddd;">${
+						item.name
+					}</td>
+          <td style="padding: 12px; border-bottom: 1px solid #ddd;">${
+						item.quantity
+					}</td>
+          <td style="padding: 12px; border-bottom: 1px solid #ddd;">${
+						item.price * item.quantity
+					}</td>
+        </tr>`;
+	});
+
+	let sum = 0;
+	cart.map((item) => (sum += item.price * item.quantity));
+
+	cartHtml += `
+      </tbody>
+    	</table>
+    <p style="font-family: Arial, sans-serif; color: #777; margin-top: 20px;">Thank you for shopping with us. Your Total cart price is <strong>$${sum} </strong> </p>`;
+
 	const transporter = nodemailer.createTransport({
 		host: 'smtp-mail.outlook.com',
 		port: 587,
-		secure: false, // Use TLS
+		secure: false,
 		auth: {
 			user: process.env.USER,
 			pass: process.env.PASS,
@@ -28,15 +64,14 @@ app.get('/api/sendmail', (req, res) => {
 		},
 	});
 
-	// Define the email options
 	const mailOptions = {
 		from: 'prathamkhandelwal1320@outlook.com',
 		to: userEmail,
 		subject: 'Walmart Cart Summary',
 		text: mailBody,
+		html: cartHtml,
 	};
 
-	// Send the email
 	transporter
 		.sendMail(mailOptions)
 		.then((info) => {
